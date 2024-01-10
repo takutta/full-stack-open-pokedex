f946fd513e819e6ef62914a760021331564dcc76
takutta <jaaxtra@gmail.com>
2024-01-10 14:48:01 +0200

    Health check added

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-app.get('/version', (req, res) => {
-  res.send('1')
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
