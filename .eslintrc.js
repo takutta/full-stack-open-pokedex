ca1410164513cdf41abcc5b499c7c024976361a9
takutta <jaaxtra@gmail.com>
2024-01-10 12:23:08 +0200

    Added new e2e test

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:cypress/recommended"
+
   ],
   "parserOptions": {
     "ecmaFeatures": {
